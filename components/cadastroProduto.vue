28426d591c504b3a1cc0c8d3746bf86b6ee076c9
jhoemauricio <jhosephmauriciojhoseph@gmail.com>
2023-09-19 15:04:08 -0300

    ajuste em cadastro label

--- components/cadastroProduto.vue
+++ components/cadastroProduto.vue
@@ -10,25 +10,25 @@
 
                 <div>
                    
-                        <label class="font-bold flex justify-center items-center">Nome</label>
+                        <label class="mb-2 text-sm font-bold">Nome</label>
                    
-                             <input class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                             <input class="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                            
                     
                 </div>
 
                 <div>
                     
-                        <label class="font-bold flex justify-center items-center">Modalidade</label>
-                        <input class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label class="mb-2 text-sm font-bold ">Modalidade</label>
+                        <input class="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                            
                     
                 </div>
 
                 <div>
                   
-                        <label class="font-bold flex justify-center items-center">Preço</label>
-                        <input class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                        <label class="mb-2 text-sm font-bold ">Preço</label>
+                        <input class="w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                            
                     
                 </div>
