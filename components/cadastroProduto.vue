4b850de70ba6c5bb8c8547ac1bced35ab9abe979
jhoemauricio <jhosephmauriciojhoseph@gmail.com>
2023-09-19 14:49:28 -0300

    responsividade tela cadastro bike

--- components/cadastroProduto.vue
+++ components/cadastroProduto.vue
@@ -2,38 +2,37 @@
 </script>
 <template>
     <div>
-        <div class="mt-8 grid grid-cols-4 gap-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
+        <div class="mt-8 grid grid-cols-3 gap-6 p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 mb-4">
 
-                <div class="col-span-4 flex justify-center items-center">
+                <div class="p-2 col-span-3 flex justify-center items-center">
                     <img class="rounded-full w-24 h-24 object-cover" src="../public/imagemTeste.jpg">
                 </div>
 
                 <div>
-                    <div>
-                        <label class="font-bold">Nome</label>
-                        <div>
-                           <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Nome">
-                        </div>
-                    </div>
+                
+                        <label class="block font-bold text-sm">Nome</label>
+                       
+                           <input class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Nome">
+                      
+                    
                 </div>
 
                 <div>
-                    <div>
-                        <label class="font-bold">Modalidade</label>
-                        <div>
-                            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Modalidade">
-                        </div>
-                    </div>
+                   
+                        <label class="block font-bold text-sm">Modalidade</label>
+                       
+                            <input class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Modalidade">
+               
                 </div>
 
                 <div>
-                    <div>
-                        <label class="font-bold">Preço</label>
-                        <div>
-                            <input class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block  pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Preço">
-                        </div>
-                    </div>
+                   
+                   <label class="block font-bold text-sm">Preço</label>
+                  
+                       <input class="w-full bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block   dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" type="text" placeholder="Preço">
+          
                 </div>
+
               
         </div>
     </div>
